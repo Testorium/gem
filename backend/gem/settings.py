45be08666ba781c0412149abe6608e367e5ffca2
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2025-05-23 15:53:24 +0500

    feat: add swagger

--- backend/gem/settings.py
+++ backend/gem/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "apps.users.apps.UsersConfig",
     # Third party
     "rest_framework",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- backend/gem/urls.py
+++ backend/gem/urls.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Goods Exchange Marketplace API",
+        default_version="0.0.1",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth", include("apps.authentication.urls")),
-    path("api/users", include("apps.users.urls")),
+    path("api/auth/", include("apps.authentication.urls")),
+    path("api/users/", include("apps.users.urls")),
+    # swagger
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
